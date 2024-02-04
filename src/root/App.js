379d46bb64a428b0ff6a4442fa9208ddc8937f3d
AkmalxonWeBd <141171275+AkmalxonWeBd@users.noreply.github.com>
2024-02-04 14:31:17 +0500

    salom

--- src/root/App.js
+++ src/root/App.js
@@ -1,15 +1,16 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Login from '../login/login';
+import Home from '../components/admin/sidebar/Sidebar';
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <Routes>
-        <Route path='login' element={<Login />} />
-        {/* <Route path='admin' element={}/> */}
-      </Routes>
-    </BrowserRouter>
+        <Routes>
+          <Route path='login' element={<Login />} />
+          <Route path='admin/home' element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
