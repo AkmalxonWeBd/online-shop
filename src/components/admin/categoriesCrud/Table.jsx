196a493418ba7ca9e1bec955129a694fb417af9d
AkmalxonWeBd <141171275+AkmalxonWeBd@users.noreply.github.com>
2024-02-07 19:20:52 +0500

    salom

--- src/components/admin/categoriesCrud/Table.jsx
+++ src/components/admin/categoriesCrud/Table.jsx
@@ -8,32 +8,7 @@ export default function CategoriesCrud({ categories, onSearch, onDelete, onUpdat
   return (
     <div>
       <Mock />
-      <div>
-        <div>
-          <input type="text" placeholder="Izlash..." onChange={(e) => onSearch(e.target.value)} />
-          <table>
-            <thead>
-              <tr>
-                <th>Category Image</th>
-                <th>Category Name</th>
-                <th>Actions</th>
-              </tr>
-            </thead>
-            <tbody>
-              {categories?.map(category => (
-                <tr key={category.id}>
-                  <td><img src={category.imageUrl} alt={category.name} /></td>
-                  <td>{category.name}</td>
-                  <td>
-                    <UpdateCategories categoryId={category.id} onUpdate={onUpdate} />
-                    <DeleteCategories categoryId={category.id} onDelete={onDelete} />
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      </div>
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat debitis dolore necessitatibus similique quia. Enim ut dicta omnis, assumenda modi, odio velit vitae magni laudantium pariatur asperiores. Sit vero omnis, ipsum rem maxime corrupti asperiores maiores optio possimus eligendi impedit voluptates, harum explicabo ad eius. Veniam, sed saepe? Nulla explicabo sunt dolorem optio placeat obcaecati totam molestiae, aut, eum rem neque deserunt illo saepe. Esse provident exercitationem non, quae similique sint, sunt laboriosam aperiam blanditiis modi saepe nesciunt commodi assumenda corporis voluptate quam adipisci dolores laborum dicta quis dolorem nisi ex harum ut. Alias laboriosam ratione, libero fugit assumenda cupiditate quo minus magnam. Facere odio consequatur omnis! Autem, sint earum? Consequuntur dolore in libero quis voluptatibus ducimus explicabo corrupti voluptatem asperiores optio sequi deserunt, corporis sit numquam doloremque magnam iure eligendi autem possimus repellendus nemo eveniet aliquam eius? Quidem nesciunt placeat molestiae suscipit consectetur sequi magni. Nihil quam eaque in placeat, libero sint earum architecto assumenda ad porro unde nulla atque ducimus provident tempora ipsam repellendus nisi consequatur qui aliquam ab accusantium impedit officiis voluptatum. A quisquam, pariatur culpa quis dolorem nam laborum rerum atque repellendus deleniti temporibus eius fugit unde harum dolores aspernatur modi, doloremque sequi qui ea deserunt explicabo doloribus incidunt. Cumque similique totam esse impedit error repellendus enim ipsa illo, corporis explicabo beatae a ducimus dignissimos molestias voluptas labore consectetur deleniti tempore earum eligendi ipsam vitae? Minima, officiis accusantium perferendis eligendi dicta incidunt quibusdam eveniet iusto rerum magni id unde. Harum, reprehenderit ducimus! Consectetur, ut non aut eaque expedita optio voluptatibus! Laboriosam accusantium quia cupiditate soluta consectetur voluptatem deleniti, recusandae doloribus pariatur, libero neque impedit placeat vitae ipsam obcaecati iusto! Veniam aut assumenda libero consequuntur perspiciatis dolores, cumque fugiat, quaerat consectetur, distinctio numquam! Ipsa mollitia doloremque aspernatur. Repellendus ea sed, hic veniam blanditiis dolores odit accusamus! Saepe quia molestias ullam omnis iusto autem, reiciendis quisquam perferendis alias natus. Facilis et cumque culpa voluptates, quaerat porro, quam blanditiis accusamus itaque ab quibusdam ipsam. Odit, ex aut? Ea nam architecto, tempore est reprehenderit porro quia temporibus, iusto earum quaerat, commodi nisi culpa necessitatibus mollitia numquam iure illo sapiente velit id nesciunt exercitationem dolorum. Hic libero quis ipsum excepturi, quaerat temporibus reprehenderit eos illo adipisci ducimus labore esse repellendus minus dolor. Porro, neque a? Dicta culpa dolore beatae tempore nulla quia officia nisi eaque vero cum, aspernatur libero numquam fuga doloremque doloribus vel et exercitationem eum magnam similique rem!</h1>
     </div>
   );
 }
